ffbe9f1513ff25c30e0174acb22bf88509fd39f8
camilov87 <55037354+camilov87@users.noreply.github.com>
2020-04-07 21:51:43 -0400

    Update Digito.py

--- Digito.py
+++ Digito.py
@@ -5,12 +5,31 @@ import time
 from urllib import request
 import json
 
+class Getclima:
+    def __init__(self):
+        self.api= "1bf90c1dc3e17f0405996dd6a0e8c0cf"
+        self.url= "http://api.openweathermap.org/data/2.5/weather?q=Curico&appid=" + self.api + ""
+    def getwebdata(self):
+        meteo = request.urlopen(self.url)
+        meteo = meteo.read().decode('utf-8')
+        diccio = json.loads(meteo)
+        return diccio
+    def gettemp(self,dic):
+        self.temp=dic["main"]["temp"]
+        return self.temp
+    def getciudad(self,dic):
+        ciudad = dic["name"]
+        return ciudad
+    def getestado(self,dic):
+        estado= dic["weather"][0]["main"]
+        return estado
+
 def GetClima():
     apikey = "1bf90c1dc3e17f0405996dd6a0e8c0cf"
     url = "http://api.openweathermap.org/data/2.5/weather?q=Curico&appid=" + apikey + ""
     meteo = request.urlopen(url)
-    # meteo= meteo.read().decode('utf-8')
-    diccio = json.load(meteo)
+    meteo= meteo.read().decode('utf-8')
+    diccio = json.loads(meteo)
     temp = diccio["main"]["temp"]
     ciudad = diccio["name"]
     print(ciudad)
@@ -19,16 +38,16 @@ def GetClima():
     return temp
 
 def num(digito, pos):
-        y=pos*150
-        S1=(y+10,20,Vert)
-        S2=(y+10,130,Vert)
-        S3=(y+120,20,Vert)
-        S4=(y+120,130,Vert)
-        S5=(y+25,5,Hor)
-        S6=(y+25,115,Hor)
-        S7=(y+25,225,Hor)
-        PS=(y+70,70,Punt)
-        PI=(y+70,180,Punt)
+        y=pos*90
+        S1=(y+10,15,Vert)
+        S2=(y+10,80,Vert)
+        S3=(y+80,15,Vert)
+        S4=(y+80,80,Vert)
+        S5=(y+20,5,Hor)
+        S6=(y+20,70,Hor)
+        S7=(y+20,140,Hor)
+        PS=(y+40,30,Punt)
+        PI=(y+40,120,Punt)
         numeros= {
             "1":(S3,S4),
             "2":(S2,S3,S5,S6,S7),
@@ -46,7 +65,26 @@ def num(digito, pos):
             image = canvas.create_image(ind[0], ind[1], anchor=NW, image=ind[2])
         return
 
-temp= str(GetClima())+" °C"
+def tiempo(test):
+    Line1=(-90, -30)
+    Line2=(-90,-200)
+    Desp=(200,180)
+    TI={"chubasco": (Line1), "tormenta":(Line1[0]-Desp[0],Line1[1]), "Clouds":(Line1[0]-Desp[0]*2,Line1[1]),
+        "nieve": (Line2), "Clear":(Line2[0]-Desp[0],Line2[1]),"Rain":(Line2[0]-Desp[0]*2,Line2[1])   }
+    return TI[test]
+
+class subcanvas:
+    def __init__(self, bigcanvas,xpos,ypos,ancho,alto):
+        self.bigcanvas=bigcanvas
+        self.xpos=xpos
+        self.ypos=ypos
+        self.subcuadro=Canvas(bigcanvas,highlightthickness=0,bg="green", width=ancho, height=alto)
+    def abrevent(self):
+        self.bigcanvas.create_window(self.xpos, self.ypos, window=self.subcuadro)
+    def creaimg(self, fotox, fotoy,imagen):
+        self.subcuadro.create_image(fotox,fotoy, anchor=NW, image=imagen)
+
+
 
 root = Tk()
 Hor = PhotoImage(file="Dig.png")
@@ -54,25 +92,29 @@ Vert = PhotoImage(file="DigVer.png")
 Punt= PhotoImage(file="Punto.png")
 Clima = PhotoImage(file="clima.png")
 
-canvas = Canvas(root, bd=0,bg="black", width=1200, height=500)
+wi=800
+hi=600
+clima= Getclima()
+dicDato= clima.getwebdata()
+ciudad= clima.getciudad(dicDato)
+temp= int(clima.gettemp(dicDato)-273.15)
+temp= str(temp)+" °C"
+
+canvas = Canvas(root, bd=0,bg="black", width=wi, height=hi)
 canvas.pack() # this makes it visible
 canvas2 = Canvas(canvas, highlightthickness=0,bg="green", width=180, height=180)
-
-def tiempo(test):
-    Line1=(-90, -30)
-    Line2=(-90,-200)
-    Desp=(200,180)
-    TI={"chubasco": (Line1), "tormenta":(Line1[0]-Desp[0],Line1[1]), "nublado":(Line1[0]-Desp[0]*2,Line1[1]),
-        "nieve": (Line2), "soleado":(Line2[0]-Desp[0],Line2[1]),"lluvia":(Line2[0]-Desp[0]*2,Line2[1])   }
-    return TI[test]
+IClima= subcanvas(canvas,wi*0.5,hi*0.6,180,180)
+icon=tiempo(clima.getestado(dicDato))
 
 def Ren():
     canvas.delete(ALL)
     hora = time.strftime('%H:%M:%S')
-    canvas.create_window(700, 400, window=canvas2)
-    canvas.create_text(1000, 450, fill="white", font=('arial',40, "bold"), text=temp)
-    icon=tiempo("tormenta")
-    canvas2.create_image(icon[0],icon[1], anchor=NW, image=Clima)
+    #canvas.create_window(700, 400, window=canvas2)  #Crea canvas para icono2
+    IClima.abrevent()
+    canvas.create_text(wi * 0.80, hi * 0.55, fill="white", font=('arial', 50, "bold"), text=ciudad)
+    canvas.create_text(wi*0.80, hi*0.65, fill="white", font=('arial',50, "bold"), text=temp)
+    IClima.creaimg(icon[0],icon[1],Clima)
+    #canvas2.create_image(icon[0],icon[1], anchor=NW, image=Clima) #
     for x in range(len(hora)):
         num(hora[x],x)
     canvas.after(200, Ren)
